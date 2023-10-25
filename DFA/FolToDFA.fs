1c9ca5688ec85a422fce45fb5b1448da4e40979a
Anya497 <chi.vinny0702@gmail.com>
2023-10-25 21:42:04 +0300

    reformat code

--- DFA/FolToDFA.fs
+++ DFA/FolToDFA.fs
@@ -3,20 +3,20 @@
 open Fol
 open PeanutProver.Automata
 
-let convertAtom (atom:Atom) =
+let convertAtom (atom: Atom) =
     match atom with
-    | Less (left, right) -> PredefinedAutomata.dfa_less
-    | Equals (left, right) -> PredefinedAutomata.dfa_eq
+    | Less(left, right) -> PredefinedAutomata.dfa_less
+    | Equals(left, right) -> PredefinedAutomata.dfa_eq
     | e -> failwithf $"Unsupported atom {e}"
 
-let rec buildProver (ast:Literal) =
+let rec buildProver (ast: Literal) =
     match ast with
     | BareAtom a -> convertAtom a
-    | Or (left, right) ->
+    | Or(left, right) ->
         let dfa_left = buildProver left
         let dfa_right = buildProver right
         DFA.union dfa_left dfa_right
-    | And (left, right) ->
+    | And(left, right) ->
         let dfa_left = buildProver left
         let dfa_right = buildProver right
         DFA.intersection dfa_left dfa_right
--- DFA/PredefinedAutomata.fs
+++ DFA/PredefinedAutomata.fs
@@ -3,8 +3,8 @@
 open PeanutProver.Automata
 
 let dfa_eq =
-    let startState = State<_,_>("eq:Start", true, true)
-    let q_1 = State<_,_>("eq:q_1", false, false)
+    let startState = State<_, _>("eq:Start", true, true)
+    let q_1 = State<_, _>("eq:q_1", false, false)
     startState.AddTransition (0, 0) startState
     startState.AddTransition (1, 1) startState
     startState.AddTransition (1, 0) q_1
@@ -14,10 +14,10 @@ let dfa_eq =
     q_1.AddTransition (0, 1) q_1
     q_1.AddTransition (1, 1) q_1
     DFA startState
-   
+
 let dfa_less =
-    let startState = State<_,_>("less:Start", true, false)
-    let q_1 = State<_,_>("less:q_1", false, true)
+    let startState = State<_, _>("less:Start", true, false)
+    let q_1 = State<_, _>("less:q_1", false, true)
     startState.AddTransition (0, 1) q_1
     startState.AddTransition (1, 1) startState
     startState.AddTransition (1, 0) startState
@@ -27,11 +27,11 @@ let dfa_less =
     q_1.AddTransition (1, 1) q_1
     q_1.AddTransition (0, 0) q_1
     DFA startState
-  
+
 let dfa_sum =
-    let noCarry = State<_,_>("sum:noCarry", true, true)
-    let carry = State<_,_>("sum:carry", false, false)
-    let q_1 = State<_,_>("sum:q_1", false, false)
+    let noCarry = State<_, _>("sum:noCarry", true, true)
+    let carry = State<_, _>("sum:carry", false, false)
+    let q_1 = State<_, _>("sum:q_1", false, false)
     noCarry.AddTransition (0, 0, 0) noCarry
     noCarry.AddTransition (0, 1, 1) noCarry
     noCarry.AddTransition (1, 0, 1) noCarry
@@ -61,4 +61,3 @@ let dfa_sum =
 let dfa_not_eq = DFA.complement dfa_eq
 
 let dfa_less_eq = DFA.union dfa_eq dfa_less
-
