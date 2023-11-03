8a1119a80970ecfae5dc438b28a63058bbdd573c
Nikolai Ponomarev <wowasterdev@gmail.com>
2023-11-03 11:54:16 +0300

    Use chars instead of ints

--- src/PeanutProver.CLI/Program.fs
+++ src/PeanutProver.CLI/Program.fs
@@ -9,7 +9,7 @@ let prover =
     | Success(ast, _, _) -> FolToDFA.buildProver ast
     | Failure(_, _, _) -> failwithf "Parser error!"
 
-prover.Recognize [ [ 1; 1; 1 ]; [ 0; 0; 0 ]; [ 0; 0; 0 ] ]
+prover.Recognize [ [ '1'; '1'; '1' ]; [ '0'; '0'; '0' ]; [ '0'; '0'; '0' ] ]
 |> fun x -> printfn $"{x}"
 
 printfn $"{ast}"
