ab85db691d08028b86b7825c01edb64669e61439
Alexey Shlyonskikh <dedantysag2@yandex.ru>
2023-11-22 19:34:54 +0300

    Use simpler DFA

--- src/DFA/PredefinedAutomata.fs
+++ src/DFA/PredefinedAutomata.fs
@@ -4,82 +4,118 @@ open PeanutProver.Automata
 open Ast.Common
 
 let dfa_eq =
-    let startState = State<_>("eq:Start", true, true)
-    let q_1 = State<_>("eq:q_1", false, false)
-    startState.AddTransition [ Zero; Zero ] startState
-    startState.AddTransition [ One; One ] startState
-    startState.AddTransition [ One; Zero ] q_1
-    startState.AddTransition [ Zero; One ] q_1
-    q_1.AddTransition [ Zero; Zero ] q_1
-    q_1.AddTransition [ One; Zero ] q_1
-    q_1.AddTransition [ Zero; One ] q_1
-    q_1.AddTransition [ One; One ] q_1
-    DFA startState
+    let startState = { Name = "eq:Start"; Id = 0 }
+    let failedState = { Name = "eq:Failed"; Id = 1 }
+    let finalStates = set [ startState ]
+
+    let transitions =
+        Map
+            [ startState,
+              Map
+                  [ [ Zero; Zero ], startState
+                    [ One; One ], startState
+                    [ One; Zero ], failedState
+                    [ Zero; One ], failedState ]
+              failedState,
+              Map
+                  [ [ Zero; Zero ], failedState
+                    [ One; One ], failedState
+                    [ One; Zero ], failedState
+                    [ Zero; One ], failedState ] ]
+
+    DFA(startState, finalStates, transitions)
 
 let dfa_less =
-    let startState = State<_>("less:Start", true, false)
-    let q_1 = State<_>("less:q_1", false, true)
-    startState.AddTransition [ Zero; One ] q_1
-    startState.AddTransition [ One; One ] startState
-    startState.AddTransition [ One; Zero ] startState
-    startState.AddTransition [ Zero; Zero ] startState
-    q_1.AddTransition [ One; Zero ] startState
-    q_1.AddTransition [ Zero; One ] q_1
-    q_1.AddTransition [ One; One ] q_1
-    q_1.AddTransition [ Zero; Zero ] q_1
-    DFA startState
+    let startState = { Name = "less:Start"; Id = 0 }
+    let lessState = { Name = "less:Less"; Id = 1 }
+    let finalStates = set [ lessState ]
+
+    let transitions =
+        Map
+            [ startState,
+              Map
+                  [ [ Zero; Zero ], startState
+                    [ One; One ], startState
+                    [ One; Zero ], startState
+                    [ Zero; One ], lessState ]
+              lessState,
+              Map
+                  [ [ Zero; Zero ], lessState
+                    [ One; One ], lessState
+                    [ One; Zero ], startState
+                    [ Zero; One ], lessState ] ]
+
+    DFA(startState, finalStates, transitions)
 
 let dfa_sum =
-    let noCarry = State<_>("sum:noCarry", true, true)
-    let carry = State<_>("sum:carry", false, false)
-    let q_1 = State<_>("sum:q_1", false, false)
-    noCarry.AddTransition [ Zero; Zero; Zero ] noCarry
-    noCarry.AddTransition [ Zero; One; One ] noCarry
-    noCarry.AddTransition [ One; Zero; One ] noCarry
-    noCarry.AddTransition [ One; One; Zero ] carry
-    noCarry.AddTransition [ Zero; One; Zero ] q_1
-    noCarry.AddTransition [ One; Zero; Zero ] q_1
-    noCarry.AddTransition [ Zero; Zero; One ] q_1
-    noCarry.AddTransition [ One; One; One ] q_1
-    q_1.AddTransition [ Zero; Zero; Zero ] q_1
-    q_1.AddTransition [ Zero; Zero; One ] q_1
-    q_1.AddTransition [ Zero; One; Zero ] q_1
-    q_1.AddTransition [ Zero; One; One ] q_1
-    q_1.AddTransition [ One; Zero; Zero ] q_1
-    q_1.AddTransition [ One; Zero; One ] q_1
-    q_1.AddTransition [ One; One; Zero ] q_1
-    q_1.AddTransition [ One; One; One ] q_1
-    carry.AddTransition [ Zero; Zero; One ] noCarry
-    carry.AddTransition [ Zero; Zero; Zero ] q_1
-    carry.AddTransition [ Zero; One; One ] q_1
-    carry.AddTransition [ One; One; Zero ] q_1
-    carry.AddTransition [ One; Zero; One ] q_1
-    carry.AddTransition [ Zero; One; Zero ] carry
-    carry.AddTransition [ One; Zero; Zero ] carry
-    carry.AddTransition [ One; One; One ] carry
-    DFA noCarry
+    let noCarry = { Name = "sum:noCarry"; Id = 0 }
+    let carry = { Name = "sum:carry"; Id = 1 }
+    let fail = { Name = "sum:fail"; Id = 2 }
+    let finalStates = set [ noCarry ]
+
+    let transitions =
+        Map
+            [ noCarry,
+              Map
+                  [ [ Zero; Zero; Zero ], noCarry
+                    [ Zero; Zero; One ], fail
+                    [ Zero; One; Zero ], fail
+                    [ Zero; One; One ], noCarry
+                    [ One; Zero; Zero ], fail
+                    [ One; Zero; One ], noCarry
+                    [ One; One; Zero ], carry
+                    [ One; One; One ], fail ]
+              carry,
+              Map
+                  [ [ Zero; Zero; Zero ], fail
+                    [ Zero; Zero; One ], noCarry
+                    [ Zero; One; Zero ], carry
+                    [ Zero; One; One ], fail
+                    [ One; Zero; Zero ], carry
+                    [ One; Zero; One ], fail
+                    [ One; One; Zero ], fail
+                    [ One; One; One ], carry ]
+              fail,
+              Map
+                  [ [ Zero; Zero; Zero ], fail
+                    [ Zero; Zero; One ], fail
+                    [ Zero; One; Zero ], fail
+                    [ Zero; One; One ], fail
+                    [ One; Zero; Zero ], fail
+                    [ One; Zero; One ], fail
+                    [ One; One; Zero ], fail
+                    [ One; One; One ], fail ] ]
+
+    DFA(noCarry, finalStates, transitions)
 
 let dfa_not_eq = DFA.complement dfa_eq
 
 let dfa_less_eq = DFA.union dfa_eq dfa_less
 
 let dfa_bitwise_minimum =
-    let start = State<_>("bm:Start", true, true)
-    let q_1 = State<_>("bm:q_1", false, false)
-    start.AddTransition [ Zero; Zero; Zero ] start
-    start.AddTransition [ Zero; One; One ] q_1
-    start.AddTransition [ One; Zero; One ] q_1
-    start.AddTransition [ One; One; Zero ] q_1
-    start.AddTransition [ Zero; One; Zero ] start
-    start.AddTransition [ One; Zero; Zero ] start
-    start.AddTransition [ Zero; Zero; One ] q_1
-    start.AddTransition [ One; One; One ] start
-    q_1.AddTransition [ Zero; Zero; Zero ] q_1
-    q_1.AddTransition [ Zero; Zero; One ] q_1
-    q_1.AddTransition [ Zero; One; Zero ] q_1
-    q_1.AddTransition [ Zero; One; One ] q_1
-    q_1.AddTransition [ One; Zero; Zero ] q_1
-    q_1.AddTransition [ One; Zero; One ] q_1
-    q_1.AddTransition [ One; One; Zero ] q_1
-    q_1.AddTransition [ One; One; One ] q_1
-    DFA start
+    let start = { Name = "bm:start"; Id = 0 }
+    let fail = { Name = "bm:fail"; Id = 1 }
+    let finalStates = set [ start ]
+
+    let transitions =
+        Map
+            [ start,
+              Map[[ Zero; Zero; Zero ], start
+                  [ Zero; Zero; One ], fail
+                  [ Zero; One; Zero ], start
+                  [ Zero; One; One ], fail
+                  [ One; Zero; Zero ], start
+                  [ One; Zero; One ], fail
+                  [ One; One; Zero ], fail
+                  [ One; One; One ], start]
+              fail,
+              Map[[ Zero; Zero; Zero ], fail
+                  [ Zero; Zero; One ], fail
+                  [ Zero; One; Zero ], fail
+                  [ Zero; One; One ], fail
+                  [ One; Zero; Zero ], fail
+                  [ One; Zero; One ], fail
+                  [ One; One; Zero ], fail
+                  [ One; One; One ], fail] ]
+
+    DFA(start, finalStates, transitions)
