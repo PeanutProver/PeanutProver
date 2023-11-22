0e533b1bd53fba14e5d30d08590f4eb5693db939
Alexey Shlyonskikh <dedantysag2@yandex.ru>
2023-11-22 22:50:35 +0300

    Handle variables correctly

--- src/DFA/FolToDFA.fs
+++ src/DFA/FolToDFA.fs
@@ -2,6 +2,7 @@
 
 open Ast.Ast
 open PeanutProver.Automata
+open PeanutProver.DFA.Common
 
 let convertAtom atom =
     match atom with
@@ -34,17 +35,44 @@ let convertAtom atom =
         | e -> failwithf $"Unsupported term {e}"
     | e -> failwithf $"Unsupported atom {e}"
 
+let makeCompatible transitions1 vars1 transitions2 vars2 =
+    let new_vars = [ vars1; vars2 ] |> List.concat |> Seq.distinct |> List.ofSeq
+    let var_number = new_vars.Length
+    let indices1 = List.map (fun var -> List.findIndex ((=) var) new_vars) vars1
+    let indices2 = List.map (fun var -> List.findIndex ((=) var) new_vars) vars2
+    let transitions1 = renumber_transitions transitions1 var_number indices1
+    let transitions2 = renumber_transitions transitions2 var_number indices2
+    transitions1, transitions2, new_vars
+
 let rec buildProver ast =
     match ast with
     | BareAtom a -> convertAtom a
     | Or(left, right) ->
         let dfa_left, left_vars = buildProver left
         let dfa_right, right_vars = buildProver right
-        DFA.union dfa_left dfa_right, left_vars
+
+        let transitions_left, transitions_right, new_vars =
+            makeCompatible dfa_left.Transitions left_vars dfa_right.Transitions right_vars
+
+        let new_dfa_left = DFA(dfa_left.StartState, dfa_left.FinalStates, transitions_left)
+
+        let new_dfa_right =
+            DFA(dfa_right.StartState, dfa_right.FinalStates, transitions_right)
+
+        DFA.union new_dfa_left new_dfa_right, new_vars
     | And(left, right) ->
         let dfa_left, left_vars = buildProver left
         let dfa_right, right_vars = buildProver right
-        DFA.intersection dfa_left dfa_right, left_vars
+
+        let transitions_left, transitions_right, new_vars =
+            makeCompatible dfa_left.Transitions left_vars dfa_right.Transitions right_vars
+
+        let new_dfa_left = DFA(dfa_left.StartState, dfa_left.FinalStates, transitions_left)
+
+        let new_dfa_right =
+            DFA(dfa_right.StartState, dfa_right.FinalStates, transitions_right)
+
+        DFA.intersection new_dfa_left new_dfa_right, new_vars
     | Not expr ->
         let dfa_expr, vars = buildProver expr
         DFA.complement dfa_expr, vars
