422573b8f60b30ddf8901ceca1d7ab6d2d386fa6
Alexey Shlyonskikh <dedantysag2@yandex.ru>
2023-11-22 19:34:54 +0300

    Use simpler DFA

--- src/DFA/DFA.fs
+++ src/DFA/DFA.fs
@@ -1,99 +1,54 @@
 ﻿namespace PeanutProver.Automata
 
-open System.Collections.Generic
-
 open Ast.Common
 
-type State<'a>(id: 'a, isStart: bool, isFinal: bool) =
-    let transitions = Dictionary<bit list, State<'a>>()
+type State = { Name: string; Id: int }
 
-    member this.AddTransition tupleOfChars state =
-        if transitions.ContainsKey(tupleOfChars) then
-            failwithf $"Transition for symbol {tupleOfChars} already exists!"
-
-        transitions.Add(tupleOfChars, state)
-
-    member this.IsStart = isStart
-    member this.IsFinal = isFinal
-    member this.Id = id
-
-    member this.GoesTo char =
-        let exists, v = transitions.TryGetValue char
-        if exists then Some v else None
-
-    override this.ToString() = string this.Id
-
-    member this.GetAllTransitions() =
-        [ for kvp in transitions -> kvp.Key, kvp.Value ]
-
-
-type Result<'a> =
+type Result =
     | Accept
     | Partial of bit list list
-    | Fail of State<'a>
+    | Fail of State
 
-[<Struct>]
-type Configuration<'a> =
-    val CurrentState: State<'a>
-    val RestOfInput: bit list list
-
-    new(state, rest) =
-        { CurrentState = state
-          RestOfInput = rest }
-
-type DFA<'a>(startState: State<'a>) =
-    let rec step (configuration: Configuration<'a>) =
-        match configuration.RestOfInput with
+type DFA(startState, finalStates, transitions) =
+    let rec recognize input state =
+        match input with
         | [] ->
-            if configuration.CurrentState.IsFinal then
+            if Set.contains state finalStates then
                 Accept
             else
-                Fail configuration.CurrentState
-
+                Fail state
         | hd :: tl ->
-            match configuration.CurrentState.GoesTo hd with
-            | None -> Partial configuration.RestOfInput
-            | Some nextState -> step (Configuration(nextState, tl))
+            match Map.tryFind state transitions with
+            | Some wordMapping ->
+                match Map.tryFind hd wordMapping with
+                | Some nextState -> recognize tl nextState
+                | _ -> Partial input
+            | _ -> Partial input
 
     member this.StartState = startState
-    member this.Recognize str = step (Configuration(startState, str))
+    member this.FinalStates = finalStates
+    member this.Transitions = transitions
+    member this.Recognize input = recognize input startState
 
     member this.ToDot() =
         let nodes = ResizeArray<_>()
         let edges = ResizeArray<_>()
-        let mutable firstFreeNodeId = 0
 
-        let dfs (state: State<_>) =
-            let toProcess = Stack<State<_> * (int -> unit)>[(state, (fun _ -> ()))]
-            let copyOfState = Dictionary<State<_>, int>()
+        for (fromState, letterInfo) in Map.toSeq transitions do
+            let nodeColor = if fromState = startState then "green" else "white"
 
-            while toProcess.Count > 0 do
-                let state, addTransitions = toProcess.Pop()
-                let exists, newState = copyOfState.TryGetValue state
-
-                if not exists then
-                    let curNodeId = firstFreeNodeId
-                    firstFreeNodeId <- firstFreeNodeId + 1
-                    let nodeColor = if state.IsStart then "green" else "white"
-                    let nodeShape = if state.IsFinal then "doublecircle" else "circle"
-
-                    nodes.Add(
-                        $"{curNodeId} [fillcolor={nodeColor} shape={nodeShape} style=filled label=\"{state.Id}\"]"
-                    )
-
-                    addTransitions curNodeId
-
-                    for (char, targetState) in state.GetAllTransitions() do
-                        toProcess.Push(
-                            targetState,
-                            (fun newTargetStateId -> edges.Add($"{curNodeId} -> {newTargetStateId} [label=\"{char}\"]"))
-                        )
-
-                    copyOfState.Add(state, curNodeId)
+            let nodeShape =
+                if Set.contains fromState finalStates then
+                    "doublecircle"
                 else
-                    addTransitions newState
+                    "circle"
 
-        dfs this.StartState
+            nodes.Add(
+                $"{fromState.Id} [fillcolor={nodeColor} shape={nodeShape} style=filled label=\"{fromState.Name}\"]"
+            )
+
+            for (letter, toState) in Map.toSeq letterInfo do
+                edges.Add($"{fromState.Id} -> {toState.Id} [label=\"{letter}\"]")
 
         seq {
             "digraph g {"
@@ -106,84 +61,42 @@ type DFA<'a>(startState: State<'a>) =
     member this.ToDot(filePath: string) =
         let dot = this.ToDot()
         System.IO.File.WriteAllText(filePath, dot)
-// member this.AllStates = allStates
-// member this.FinalStates = allStates |> Seq.filter (fun x -> x.IsFinal) |> HashSet
-// member this.Alphabet = alphabet
 
 module DFA =
-    let complement (dfa: DFA<_>) =
-        let dfs (state: State<_>) =
-            let mutable newStartState = Unchecked.defaultof<_>
-            let toProcess = Stack<State<_> * (State<_> -> unit)>[(state, (fun _ -> ()))]
-            let copyOfState = Dictionary<_, _>()
+    let complement (dfa: DFA) =
+        DFA(
+            dfa.StartState,
+            dfa.Transitions
+            |> Seq.map (fun node -> node.Key)
+            |> Seq.filter (not << dfa.FinalStates.Contains)
+            |> Set.ofSeq,
+            dfa.Transitions
+        )
 
-            while toProcess.Count > 0 do
-                let state, addTransitions = toProcess.Pop()
-                let exists, newState = copyOfState.TryGetValue state
+    let intersection (dfa1: DFA) (dfa2: DFA) =
+        let stateNumber2 = dfa2.Transitions.Count
 
-                if not exists then
-                    let newState = State<_>(state.Id, state.IsStart, not state.IsFinal)
+        let makeNewState (state1, state2) =
+            { Name = $"{state1.Name}_{state2.Name}"
+              Id = state1.Id * stateNumber2 + state2.Id }
 
-                    if newState.IsStart then
-                        newStartState <- newState
+        let startState = makeNewState (dfa1.StartState, dfa2.StartState)
 
-                    addTransitions newState
+        let transitions =
+            Seq.allPairs dfa1.Transitions dfa2.Transitions
+            |> Seq.map (fun (first, second) ->
+                (makeNewState (first.Key, second.Key),
+                 first.Value
+                 |> Seq.map (fun node -> (node.Key, makeNewState (node.Value, second.Value[node.Key])))
+                 |> Map.ofSeq))
+            |> Map.ofSeq
 
-                    for (char, targetState) in state.GetAllTransitions() do
-                        toProcess.Push(targetState, (fun newTargetState -> newState.AddTransition char newTargetState))
+        let finalStates =
+            Seq.allPairs dfa1.FinalStates dfa2.FinalStates
+            |> Seq.map makeNewState
+            |> Set.ofSeq
 
-                    copyOfState.Add(state, newState)
-                else
-                    addTransitions newState
+        DFA(startState, finalStates, transitions)
 
-            newStartState
-
-        let newStartState = dfs dfa.StartState
-        DFA(newStartState)
-
-    let intersection (dfa1: DFA<_>) (dfa2: DFA<_>) =
-        let dfs (state1: State<_>) (state2: State<_>) =
-            let mutable newStartState = Unchecked.defaultof<_>
-
-            let toProcess =
-                Stack<State<_> * State<_> * (State<_> -> unit)>[(state1, state2, (fun _ -> ()))]
-
-            let copyOfState = Dictionary<_, _>()
-
-            while toProcess.Count > 0 do
-                let state1, state2, addTransitions = toProcess.Pop()
-                let exists, newState = copyOfState.TryGetValue((state1, state2))
-
-                if not exists then
-                    let newState =
-                        State<_>(
-                            $"{state1.Id}_{state2.Id}",
-                            state1.IsStart && state2.IsStart,
-                            state1.IsFinal && state2.IsFinal
-                        )
-
-                    if newState.IsStart then
-                        newStartState <- newState
-
-                    addTransitions newState
-
-                    for (char1, targetState1) in state1.GetAllTransitions() do
-                        for (char2, targetState2) in state2.GetAllTransitions() do
-                            if char1 = char2 then
-                                toProcess.Push(
-                                    targetState1,
-                                    targetState2,
-                                    (fun newTargetState -> newState.AddTransition char1 newTargetState)
-                                )
-
-                    copyOfState.Add((state1, state2), newState)
-                else
-                    addTransitions newState
-
-            newStartState
-
-        let newStartState = dfs dfa1.StartState dfa2.StartState
-        DFA(newStartState)
-
-    let union dfa1 dfa2 =
+    let union (dfa1: DFA) (dfa2: DFA) =
         complement (intersection (complement dfa1) (complement dfa2))
