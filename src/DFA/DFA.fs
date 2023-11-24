8efc6ffd22eb1ec2b050f5f65ef3528fbbe21680
Alexey Shlyonskikh <dedantysag2@yandex.ru>
2023-11-24 15:27:31 +0300

    nfa + working exists

--- src/DFA/DFA.fs
+++ src/DFA/DFA.fs
@@ -7,38 +7,43 @@ type State = { Name: string; Id: int }
 type Result =
     | Accept
     | Partial of bit list list
-    | Fail of State
+    | Fail of State seq
 
-type DFA(startState, finalStates, transitions) =
-    let rec recognize input state =
+type DFA(startStates, finalStates: _ seq, transitions) =
+    let rec recognize input states =
         match input with
         | [] ->
-            if Set.contains state finalStates then
+            if states |> Seq.exists (fun state -> Seq.contains state finalStates) then
                 Accept
             else
-                Fail state
+                Fail states
         | hd :: tl ->
-            match Map.tryFind state transitions with
-            | Some wordMapping ->
-                match Map.tryFind hd wordMapping with
-                | Some nextState -> recognize tl nextState
-                | _ -> Partial input
-            | _ -> Partial input
+            let find state =
+                match Map.tryFind state transitions with
+                | Some(wordMapping: Map<bit list, State seq>) -> Map.tryFind hd wordMapping
+                | _ -> None
+                |> (Option.toList)
+                |> List.toSeq
+                |> Seq.concat in
 
-    member this.StartState = startState
+            states |> Seq.map (find) |> Seq.concat |> recognize tl
+
+    member this.StartState = startStates
     member this.FinalStates = finalStates
     member this.Transitions = transitions
-    member this.Recognize input = recognize input startState
+
+    member this.Recognize input =
+        recognize input <| Seq.singleton startStates
 
     member this.ToDot() =
         let nodes = ResizeArray<_>()
         let edges = ResizeArray<_>()
 
         for (fromState, letterInfo) in Map.toSeq transitions do
-            let nodeColor = if fromState = startState then "green" else "white"
+            let nodeColor = if fromState = startStates then "green" else "white"
 
             let nodeShape =
-                if Set.contains fromState finalStates then
+                if Seq.contains fromState finalStates then
                     "doublecircle"
                 else
                     "circle"
@@ -48,7 +53,7 @@ type DFA(startState, finalStates, transitions) =
             )
 
             for (letter, toState) in Map.toSeq letterInfo do
-                edges.Add($"{fromState.Id} -> {toState.Id} [label=\"%A{letter}\"]")
+                edges.Add($"{fromState.Id} -> %A{toState} [label=\"%A{letter}\"]")
 
         seq {
             "digraph g {"
@@ -68,7 +73,7 @@ module DFA =
             dfa.StartState,
             dfa.Transitions
             |> Seq.map (fun node -> node.Key)
-            |> Seq.filter (not << dfa.FinalStates.Contains)
+            |> Seq.filter (not << (fun x -> Seq.contains x dfa.FinalStates))
             |> Set.ofSeq,
             dfa.Transitions
         )
@@ -80,6 +85,9 @@ module DFA =
             { Name = $"{state1.Name}_{state2.Name}"
               Id = state1.Id * stateNumber2 + state2.Id }
 
+        let makeNewStates (fst, snd) =
+            Seq.allPairs fst snd |> Seq.map makeNewState
+
         let startState = makeNewState (dfa1.StartState, dfa2.StartState)
 
         let transitions =
@@ -87,7 +95,7 @@ module DFA =
             |> Seq.map (fun (first, second) ->
                 (makeNewState (first.Key, second.Key),
                  first.Value
-                 |> Seq.map (fun node -> (node.Key, makeNewState (node.Value, second.Value[node.Key])))
+                 |> Seq.map (fun node -> (node.Key, makeNewStates (node.Value, second.Value[node.Key])))
                  |> Map.ofSeq))
             |> Map.ofSeq
 
@@ -100,3 +108,31 @@ module DFA =
 
     let union (dfa1: DFA) (dfa2: DFA) =
         complement (intersection (complement dfa1) (complement dfa2))
+
+    let removeManyAt ids ls =
+        let rec remove count = function
+            | hd :: tl ->
+                if List.exists ((=) count) ids then
+                    remove (count + 1) tl
+                else hd :: remove (count + 1) tl
+            | [] -> []
+        remove 0 ls
+        |> List.rev
+        
+    let projection (dfa: DFA) vars =
+        dfa.Transitions
+        |> Map.toSeq
+        |> Seq.map (fun (key, value) ->
+            value
+            |> Map.toSeq
+            |> Seq.fold (fun acc (bitV, states) ->
+                let newBit = removeManyAt vars bitV
+                Map.tryFind newBit acc
+                |> (function
+                    | None -> Map.add newBit states acc 
+                    | Some accStates -> Map.add newBit (Seq.append accStates states) acc) 
+                ) Map.empty
+            |> fun newMap -> key, newMap
+            )
+        |> fun x -> DFA(dfa.StartState, dfa.FinalStates, Map.ofSeq x)
+  
--- src/DFA/PredefinedAutomata.fs
+++ src/DFA/PredefinedAutomata.fs
@@ -3,47 +3,49 @@
 open PeanutProver.Automata
 open Ast.Common
 
+let set = Seq.singleton
+
 let dfa_eq =
     let startState = { Name = "eq:Start"; Id = 0 }
     let failedState = { Name = "eq:Failed"; Id = 1 }
-    let finalStates = set [ startState ]
+    let finalStates = set startState
 
     let transitions =
         Map
             [ startState,
               Map
-                  [ [ Zero; Zero ], startState
-                    [ One; One ], startState
-                    [ One; Zero ], failedState
-                    [ Zero; One ], failedState ]
+                  [ [ Zero; Zero ], set startState
+                    [ One; One ], set startState
+                    [ One; Zero ], set failedState
+                    [ Zero; One ], set failedState ]
               failedState,
               Map
-                  [ [ Zero; Zero ], failedState
-                    [ One; One ], failedState
-                    [ One; Zero ], failedState
-                    [ Zero; One ], failedState ] ]
+                  [ [ Zero; Zero ], set failedState
+                    [ One; One ], set failedState
+                    [ One; Zero ], set failedState
+                    [ Zero; One ], set failedState ] ]
 
     DFA(startState, finalStates, transitions)
 
 let dfa_less =
     let startState = { Name = "less:Start"; Id = 0 }
     let lessState = { Name = "less:Less"; Id = 1 }
-    let finalStates = set [ lessState ]
+    let finalStates = set lessState
 
     let transitions =
         Map
             [ startState,
               Map
-                  [ [ Zero; Zero ], startState
-                    [ One; One ], startState
-                    [ One; Zero ], startState
-                    [ Zero; One ], lessState ]
+                  [ [ Zero; Zero ], set startState
+                    [ One; One ], set startState
+                    [ One; Zero ], set startState
+                    [ Zero; One ], set lessState ]
               lessState,
               Map
-                  [ [ Zero; Zero ], lessState
-                    [ One; One ], lessState
-                    [ One; Zero ], startState
-                    [ Zero; One ], lessState ] ]
+                  [ [ Zero; Zero ], set lessState
+                    [ One; One ], set lessState
+                    [ One; Zero ], set startState
+                    [ Zero; One ], set lessState ] ]
 
     DFA(startState, finalStates, transitions)
 
@@ -51,40 +53,40 @@ let dfa_sum =
     let noCarry = { Name = "sum:noCarry"; Id = 0 }
     let carry = { Name = "sum:carry"; Id = 1 }
     let fail = { Name = "sum:fail"; Id = 2 }
-    let finalStates = set [ noCarry ]
+    let finalStates = set noCarry
 
     let transitions =
         Map
             [ noCarry,
               Map
-                  [ [ Zero; Zero; Zero ], noCarry
-                    [ Zero; Zero; One ], fail
-                    [ Zero; One; Zero ], fail
-                    [ Zero; One; One ], noCarry
-                    [ One; Zero; Zero ], fail
-                    [ One; Zero; One ], noCarry
-                    [ One; One; Zero ], carry
-                    [ One; One; One ], fail ]
+                  [ [ Zero; Zero; Zero ], set noCarry
+                    [ Zero; Zero; One ], set fail
+                    [ Zero; One; Zero ], set fail
+                    [ Zero; One; One ], set noCarry
+                    [ One; Zero; Zero ], set fail
+                    [ One; Zero; One ], set noCarry
+                    [ One; One; Zero ], set carry
+                    [ One; One; One ], set fail ]
               carry,
               Map
-                  [ [ Zero; Zero; Zero ], fail
-                    [ Zero; Zero; One ], noCarry
-                    [ Zero; One; Zero ], carry
-                    [ Zero; One; One ], fail
-                    [ One; Zero; Zero ], carry
-                    [ One; Zero; One ], fail
-                    [ One; One; Zero ], fail
-                    [ One; One; One ], carry ]
+                  [ [ Zero; Zero; Zero ], set fail
+                    [ Zero; Zero; One ], set noCarry
+                    [ Zero; One; Zero ], set carry
+                    [ Zero; One; One ], set fail
+                    [ One; Zero; Zero ], set carry
+                    [ One; Zero; One ], set fail
+                    [ One; One; Zero ], set fail
+                    [ One; One; One ], set carry ]
               fail,
               Map
-                  [ [ Zero; Zero; Zero ], fail
-                    [ Zero; Zero; One ], fail
-                    [ Zero; One; Zero ], fail
-                    [ Zero; One; One ], fail
-                    [ One; Zero; Zero ], fail
-                    [ One; Zero; One ], fail
-                    [ One; One; Zero ], fail
-                    [ One; One; One ], fail ] ]
+                  [ [ Zero; Zero; Zero ], set fail
+                    [ Zero; Zero; One ], set fail
+                    [ Zero; One; Zero ], set fail
+                    [ Zero; One; One ], set fail
+                    [ One; Zero; Zero ], set fail
+                    [ One; Zero; One ], set fail
+                    [ One; One; Zero ], set fail
+                    [ One; One; One ], set fail ] ]
 
     DFA(noCarry, finalStates, transitions)
 
@@ -95,27 +97,27 @@ let dfa_less_eq = DFA.union dfa_eq dfa_less
 let dfa_bitwise_minimum =
     let start = { Name = "bm:start"; Id = 0 }
     let fail = { Name = "bm:fail"; Id = 1 }
-    let finalStates = set [ start ]
+    let finalStates = set start
 
     let transitions =
         Map
             [ start,
-              Map[[ Zero; Zero; Zero ], start
-                  [ Zero; Zero; One ], fail
-                  [ Zero; One; Zero ], start
-                  [ Zero; One; One ], fail
-                  [ One; Zero; Zero ], start
-                  [ One; Zero; One ], fail
-                  [ One; One; Zero ], fail
-                  [ One; One; One ], start]
+              Map[[ Zero; Zero; Zero ], set start
+                  [ Zero; Zero; One ], set fail
+                  [ Zero; One; Zero ], set start
+                  [ Zero; One; One ], set fail
+                  [ One; Zero; Zero ], set start
+                  [ One; Zero; One ], set fail
+                  [ One; One; Zero ], set fail
+                  [ One; One; One ], set start]
               fail,
-              Map[[ Zero; Zero; Zero ], fail
-                  [ Zero; Zero; One ], fail
-                  [ Zero; One; Zero ], fail
-                  [ Zero; One; One ], fail
-                  [ One; Zero; Zero ], fail
-                  [ One; Zero; One ], fail
-                  [ One; One; Zero ], fail
-                  [ One; One; One ], fail] ]
+              Map[[ Zero; Zero; Zero ], set fail
+                  [ Zero; Zero; One ], set fail
+                  [ Zero; One; Zero ], set fail
+                  [ Zero; One; One ], set fail
+                  [ One; Zero; Zero ], set fail
+                  [ One; Zero; One ], set fail
+                  [ One; One; Zero ], set fail
+                  [ One; One; One ], set fail] ]
 
     DFA(start, finalStates, transitions)
